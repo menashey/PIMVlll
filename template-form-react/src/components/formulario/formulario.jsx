680496d11bb8b81cc6c9e6da8947a323d58fe229
Yasmin Menashe <yasmin.menashe@cbyk.com.br>
2022-11-20 18:44:00 -0300

    formulario react.js

--- template-form-react/src/components/formulario/formulario.jsx
+++ template-form-react/src/components/formulario/formulario.jsx
@@ -7,31 +7,31 @@ function formulario () {
         <form className="campo">
     <div className="inputarea">
         <label for="nome" className="textinput">Nome:</label>
-        <input type="text" id="nome" className="inputfield" />
+        <input type="text" id="nome" className="inputfieldname" />
     </div>
     <div className="inputarea">
         <label for="cpf" className="textinput">CPF:</label>
-        <input type="number" id="cpf" className="inputfield"/>
+        <input type="number" id="cpf" className="inputfieldcpf"/>
     </div> 
     <div className="inputarea">
         <label for="endereco" className="textinput">Endereço:</label>
-        <input type="text" id="endereco"className="inputfield" />
+        <input type="text" id="endereco"className="inputfieldadress" />
     </div>
     <div className="inputarea">
         <label for="estado" className="textinput">Estado:</label>
-        <input type="text" id="estado" className="inputfield"/>
+        <input type="text" id="estado" className="inputfieldstate"/>
     </div>
     <div className="inputarea">
         <label for="bairro" className="textinput">Bairro:</label>
-        <input type="text" id="bairro" className="inputfield"/>
+        <input type="text" id="bairro" className="inputfielddistrict"/>
     </div>
     <div className="inputarea">
         <label for="cep" className="textinput">CEP:</label>
-        <input type="number" id="cep" className="inputfield"/>
+        <input type="number" id="cep" className="inputfieldzipcode"/>
     </div>
     <div className="inputarea">
         <label for="telefone" className="textinput">Telefone:</label>
-        <input type="number" id="numero" className="inputfield"/>
+        <input type="number" id="numero" className="inputfieldnumber"/>
     </div>
     <Botoes></Botoes>
 </form>
